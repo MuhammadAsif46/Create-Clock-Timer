d8aba7d9b005d29b6896a4d37c93f73aa3baffb8
MuhammadAsif <asifahmed32c@gmail.com>
2023-03-27 01:35:02 +0500

    logics : clock timer run

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,10 @@
-var hour =  document.getElementById("hour")
-var minutes =  document.getElementById("minute")
-var seconds =  document.getElementById("seconds")
-var amPm = document.getElementById("am-pm")
+var hour =  document.getElementById("hour");
+var minutes =  document.getElementById("minute");
+var seconds =  document.getElementById("seconds");
+var amPm = document.getElementById("am-pm");
+
+
+hour.innerText = date.getHour();
+minutes.innerText = date.getMinutes();
+seconds.innerText = date.getSeconds();
+amPm.innerText = "am";
