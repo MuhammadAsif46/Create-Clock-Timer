b02671d5fabbcb49c433b9fc90feea7f37913566
MuhammadAsif <asifahmed32c@gmail.com>
2023-03-27 01:45:03 +0500

    added function & if else statement.

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,18 @@
-var hour =  document.getElementById("hour");
+var hours =  document.getElementById("hour");
 var minutes =  document.getElementById("minute");
 var seconds =  document.getElementById("seconds");
 var amPm = document.getElementById("am-pm");
 
+setInterval(function(){
+    var date = new Date()
+    var currentHour = date.getHours();
+})
 
+if (currentHour > 11) {
+    amPm.innerText = "pm"
+}if (currentHour > 12) {
+    currentHour -= 12
+}
 hour.innerText = date.getHour();
 minutes.innerText = date.getMinutes();
 seconds.innerText = date.getSeconds();
