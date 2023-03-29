a43e232c0bc67e79920800b2f59d62012dce9f7a
MuhammadAsif <asifahmed32c@gmail.com>
2023-03-30 00:37:14 +0500

    create clock timer & updated all files

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,31 @@
-var hours =  document.getElementById("hour");
-var minutes =  document.getElementById("minute");
-var seconds =  document.getElementById("seconds");
-var amPm = document.getElementById("am-pm");
+function displaytime() {
+      var date = new Date();
+      var hrs = date.getHours();
+      var min = date.getMinutes();
+      var sec = date.getSeconds();
+      var sessions = document.getElementById("session");
+      var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      var day = dayNames[date.getDay()];
+      var monthNames = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+      var month = monthNames[date.getMonth()];
+      var date = date.getDate();
 
-setInterval(function(){
-    var date = new Date()
-    var currentHour = date.getHours();
-})
 
-if (currentHour > 11) {
-    amPm.innerText = "pm"
-}if (currentHour > 12) {
-    currentHour -= 12
+      if (hrs >= 12){
+            sessions.innerHTML = "PM";
+      }else{
+            sessions.innerHTML = "AM";
+      }
+
+      if (hrs > 12){
+            hrs = hrs - 12;
+      }
+
+      document.getElementById("hour").innerHTML = hrs;
+      document.getElementById("minutes").innerHTML = min;
+      document.getElementById("seconds").innerHTML = sec;
+      document.getElementById("day").innerHTML = day;
+      document.getElementById("month").innerHTML = month;
+      document.getElementById("date").innerHTML = date;
 }
-hour.innerText = date.getHour();
-minutes.innerText = date.getMinutes();
-seconds.innerText = date.getSeconds();
-amPm.innerText = "am";
+setInterval(displaytime,10);
